642ceedc2081f1417c1ea3a3a7e906f7e9af916a
Anastasia <example@ru>
2021-03-03 20:27:53 +0300

    Init

--- src/components/Sort/Sort.tsx
+++ src/components/Sort/Sort.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import './Sort.css'
 
 
@@ -9,11 +9,29 @@ interface ISort {
 
 const Sort: React.FC<ISort> = ({ items }) => {
 
-	const [visiblePopup, setVisiblePopup] = useState(true)
+	const [visiblePopup, setVisiblePopup] = useState(false)
+	const sortRef = useRef(null)
+
+	const toggleViziblePopup = ( ) => {
+		setVisiblePopup( !visiblePopup )
+	}
+
+ const closePopup = ( e: any ) => {
+	 if ( !e.path.includes( sortRef.current )) {
+		 setVisiblePopup( false )
+	 }
+ }
+
+ useEffect( ()=> {
+	document.addEventListener( 'click', closePopup )
+ },[])
 
 
+ 
 	return (
-		<div className="sort">
+		<div 
+		ref={ sortRef }
+		className="sort">
 			<div className="sort__label">
 				<svg className={visiblePopup ? 'rotated' : ""}
 					width="10"
@@ -25,7 +43,9 @@ const Sort: React.FC<ISort> = ({ items }) => {
 						d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"
 						fill="#2C2C2C" />
 				</svg>
-				<b>Сортировка по:</b>
+				<b 
+				onClick={ toggleViziblePopup }
+				>Сортировка по:</b>
 				<span> По цене </span>
 			</div>
 			{ visiblePopup && (
